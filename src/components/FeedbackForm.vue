2f68c616cbad0232f4b5a3f98afae1ffcf833e93
Robert Newton <robert.newton11@gmail.com>
2020-03-25 18:38:43 -0700

    add feedback form

---
+++ src/components/FeedbackForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <iframe 
+            class="airtable-embed airtable-dynamic-height" 
+            src="https://airtable.com/embed/shrMGg9Jo9odCmhi4?backgroundColor=green" 
+            frameborder="0" 
+            onmousewheel="" 
+            width="100%" 
+            height="1143" 
+            style="background: transparent; border: 1px solid #ccc;"
+        ></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FeedbackForm',
+  mounted() {
+      let tag = document.createElement('script')
+      tag.setAttribute('src', 'https://static.airtable.com/js/embed/embed_snippet_v1.js')
+      document.head.appendChild(tag)
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home container">
     <ScoreSheet />
+    <FeedbackForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ScoreSheet from '@/components/ScoreSheet.vue'
+import FeedbackForm from '@/components/FeedbackForm.vue'
 
 export default {
   name: 'home',
   components: {
-    ScoreSheet
+    ScoreSheet,
+    FeedbackForm,
   }
 }
 </script>
